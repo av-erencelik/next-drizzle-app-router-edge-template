87f9441e699b24ddb038c2d2f8eb065fa50b2b42
av-erencelik <erenmemo05@gmail.com>
2023-04-13 02:11:14 +0300

    fixing webhook

--- src/pages/api/auth/user-created.js
+++ src/pages/api/auth/user-created.js
@@ -23,13 +23,11 @@ export default async function handler(req, res) {
     res.status(400).json({});
   }
 
-  if (msg.type === "user.created") {
-    db.insert(users).values({
-      id: msg.data.id,
-      username: msg.data.username,
-      email: msg.data.email_addresses[0].email_address,
-    });
-  }
+  db.insert(users).values({
+    id: msg.data.id,
+    username: msg.data.username,
+    email: msg.data.email_addresses[0].email_address,
+  });
 
   res.json({});
 }

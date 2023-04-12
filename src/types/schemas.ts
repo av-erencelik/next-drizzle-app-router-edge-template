c599200060f19154e0394825e2c69f9daeb1fd8a
av-erencelik <erenmemo05@gmail.com>
2023-04-13 00:30:26 +0300

    changed zod schemas of login

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -4,7 +4,7 @@ export const signUpSchema = z
   .object({
     username: z
       .string()
-      .min(2, "Username must be at least 2 characters")
+      .min(4, "Username must be at least 4 characters")
       .max(20, "Username must be at most 20 characters"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
@@ -14,10 +14,10 @@ export const signInSchema = z
   .object({
     identifier: z
       .string()
-      .email("Invalid email address")
-      .or(
-        z.string().min(2, "Username must be at least 2 characters").max(20, "Username must be at most 20 characters")
-      ),
+      .min(4, "Username must be at least 4 characters")
+      .max(20, "Username must be at most 20 characters")
+      .regex(/^[a-zA-Z0-9_]+$/, "Enter a valid username or email address")
+      .or(z.string().email("Invalid email address")),
     password: z.string().min(6, "Password must be at least 6 characters"),
   })
   .required();

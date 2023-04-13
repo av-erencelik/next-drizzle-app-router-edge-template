493835960dc5b69c7d0e355a40b7426b5aebbba2
av-erencelik <erenmemo05@gmail.com>
2023-04-13 03:10:07 +0300

    changed webhook api to typescript

--- src/pages/api/auth/user-created.ts
+++ src/pages/api/auth/user-created.ts
@@ -3,6 +3,8 @@ import { buffer } from "micro";
 import db from "../../../db/db";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { NextApiRequest, NextApiResponse } from "next";
+import { UserWebhook } from "@/types/types";
 
 export const config = {
   api: {
@@ -12,16 +14,17 @@ export const config = {
 
 const secret = process.env.WEBHOOK_SECRET;
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const payload = (await buffer(req)).toString();
   const headers = req.headers;
 
-  const wh = new Webhook(secret);
-  let msg;
+  const wh = new Webhook(secret!);
+  let msg: UserWebhook;
   try {
-    msg = wh.verify(payload, headers);
+    msg = wh.verify(payload, headers as any) as any;
   } catch (err) {
     res.status(400).json({});
+    return;
   }
   if (msg.type === "user.created") {
     await db.insert(users).values({
